5a769115a1b648c89bb934e54cd5e6c94e99949d
Ernesto Campos Villa <misterernest@hotmail.com>
2019-02-02 12:50:20 -0500

    modify file with example

--- lib/humano.rb
+++ lib/humano.rb
@@ -2,7 +2,14 @@
 class Humano
   attr_accessor :name, :age, :genre
 
-  def greet
-    puts "Hola, me llamo #{@name}"
+  def initialize(name, age, genre)
+    @name = name
+    @age = age
+    @genre = genre
   end
 end
+
+juan = Humano.new 'Juan', 22, 'Masculino'
+puts juan.name
+puts juan.age
+puts juan.genre
